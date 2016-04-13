e0d44f3f2f04923f71b96f7413342834c7ff6889
dchambers <dominic.chambers@gmail.com>
2016-04-13 22:42:38 +0100

    Add Flow annotations

--- src/reducers/todo-items-reducer.js
+++ src/reducers/todo-items-reducer.js
@@ -1,4 +1,37 @@
+/* @flow */
 import {compose as lens, lensIndex as i, lensProp as p, remove, set} from 'ramda';
+
+export type TodoItems = Array<TodoItem>;
+type TodoItem = {
+  text: string,
+  completed: boolean
+};
+
+type TodoAction = AddTodoAction | DeleteTodoAction | EditTodoAction |
+  ToggleTodoAction | CompleteAllAction | ClearCompletedAction;
+type AddTodoAction = {
+  type: 'ADD_TODO',
+  text: string
+};
+type DeleteTodoAction = {
+  type: 'DELETE_TODO',
+  index: number
+};
+type EditTodoAction = {
+  type: 'EDIT_TODO',
+  index: number,
+  text: string
+};
+type ToggleTodoAction = {
+  type: 'TOGGLE_TODO',
+  index: number
+};
+type CompleteAllAction = {
+  type: 'COMPLETE_ALL'
+};
+type ClearCompletedAction = {
+  type: 'CLEAR_COMPLETED'
+};
 
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
@@ -7,7 +40,7 @@ const TOGGLE_TODO = 'TOGGLE_TODO';
 const COMPLETE_ALL = 'COMPLETE_ALL';
 const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
 
-export const todoItemsReducer = (todoItems = [], action) => {
+export const todoItemsReducer = (todoItems: TodoItems = [], action: TodoAction): TodoItems => {
   switch(action.type) {
     case ADD_TODO: {
       const newTodoItem = {text: action.text, completed: false};
